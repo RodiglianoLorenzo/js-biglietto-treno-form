ed0d2a7f4f36d580e85dae5a3f000e0493fa4500
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-12 15:12:40 +0100

    overwrite the name on NomePasseggero

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ const ageGroupEl = document.getElementById("ageGroup");
 
 const formEl = document.querySelector("form")
 
+const nomePasseggeroEl = document.getElementById("NomePasseggero");
+const OffertaEl = document.getElementById("Offerta");
+const CarozzaEl = document.getElementById("Carozza");
+const codiceCP = document.getElementById("codiceCP");
+const CostoBigliettoEl = document.getElementById("CostoBiglietto");
+
+
 formEl.addEventListener("submit", function(e){
     e.preventDefault()
-    console.log(fullNameEl.value, distanceEl.value, ageGroupEl.value);
     
-})
+    const nome = fullNameEl.value;
+    const km = distanceEl.value;
+    const eta = ageGroupEl.value;
+    
+    nomePasseggeroEl.innerHTML = nome;
 
+})
